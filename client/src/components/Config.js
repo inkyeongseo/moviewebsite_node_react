129a81a823a4de3c8226d132551886070126d956
inkyeong <dlsrud0415@naver.com>
2021-09-14 18:45:55 +0900

    LandingPage

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -5,3 +5,5 @@ export const USER_SERVER = '/api/users';
 export const API_URL = 'https://api.themoviedb.org/3/';
 
 export const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p';
+
+export const API_KEY = '52d7e638cbbaf7caa75201cc5a7dfdd4';
--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import { FaCode } from "react-icons/fa";
+import {API_URL, API_KEY, IMAGE_BASE_URL} from '../../Config'
+import MainImage from './Sections/MainImage'
+
 
 function LandingPage() {
-    return (
-        <div style={{}}>
-            <div>
 
+    const [Movies, setMovies] = useState([])
+    const [MainMoiveImage, setMainMoiveImage] = useState(null)
+
+
+    useEffect(() => {
+        
+        const endPoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+
+        fetch(endPoint)
+        .then(response => response.json())
+        .then(response => {
+
+            setMovies([response.results])
+            setMainMoiveImage(response.results[0])
+        })
+
+    }, [])
+
+
+    return (
+        <div style={{width: '100%', margin: '0'}}>
+            {/* Main Image */}
+            {MainMoiveImage &&
+            <MainImage 
+                image={`${IMAGE_BASE_URL}w1280${MainMoiveImage.backdrop_path}`}
+                title ={MainMoiveImage.original_title}
+                text={MainMoiveImage.overview}
+            />
+            }   
+            <div style={{width: '85%', margin :'1rem auto'}}>
+                <h2>Movie by latest</h2>
+                <hr />
+                {/* Movie Grid Cards */}
+            </div>
+
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <button>Load More</button>
             </div>
         </div>
     )
